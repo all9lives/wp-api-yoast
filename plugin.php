4711204c6d87d988e50912d9d70b5c0837011242
bravokeyl <bravokeyl@gmail.com>
2018-02-22 18:12:00 +0530

    Use default meta-desc if post meta is empty :ok_hand:

--- plugin.php
+++ plugin.php
@@ -1,19 +1,31 @@
 <?php
 /**
- * Plugin Name: JSON REST API Yoast routes 
- * Description: Adds Yoast fields to page and post metadata 
+ * Plugin Name: JSON REST API Yoast routes
+ * Description: Adds Yoast fields to page and post metadata
  * Author: jmfurlott<jmfurlott@gmail.com>
  * Author URI: https://jmfurlott.com
  * Author: nabilfreeman<nabil+oss@freemans.website>
  * Author URI: http://freemans.website
- * Version: 1.0.0
+ * Version: 1.1.0
  * Plugin URI: https://github.com/jmfurlott/wp-api-yoast
  */
+
 function wp_api_encode_yoast($data, $post, $context) {
+    $bk_metadesc = get_post_meta($post->ID, '_yoast_wpseo_metadesc', true);
+    if(empty($bk_metadesc)) {
+      $bk_titles = get_option( 'wpseo_titles' );
+      $bk_metadesc = $bk_titles['metadesc-post'];
+      if(class_exists('WPSEO_Replace_Vars')) {
+        $bk_replacer = new WPSEO_Replace_Vars();
+        // replace template variables
+        $bk_metadesc = $bk_replacer->replace( $bk_metadesc, $post);
+      }
+    }
+
     $yoastMeta = array(
         'yoast_wpseo_focuskw' => get_post_meta($post->ID, '_yoast_wpseo_focuskw', true),
         'yoast_wpseo_title' => get_post_meta($post->ID, '_yoast_wpseo_title', true),
-        'yoast_wpseo_metadesc' => get_post_meta($post->ID, '_yoast_wpseo_metadesc', true),
+        'yoast_wpseo_metadesc' => esc_html($bk_metadesc),
         'yoast_wpseo_linkdex' => get_post_meta($post->ID, '_yoast_wpseo_linkdex', true),
         'yoast_wpseo_metakeywords' => get_post_meta($post->ID, '_yoast_wpseo_metakeywords', true),
         'yoast_wpseo_meta-robots-noindex' => get_post_meta($post->ID, '_yoast_wpseo_meta-robots-noindex', true),
